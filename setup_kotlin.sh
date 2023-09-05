835f27f42cd7c75e1b281f3fe01f6e9bac17cf2f
Victor Ng <v.ng@glossgenius.com>
2023-09-05 12:48:45 -0400

    setup kotlin

---
+++ setup_kotlin.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+######################################################################
+# @author      : vng (vng@$HOSTNAME)
+# @file        : setup_kotlin
+# @created     : Tuesday Sep 05, 2023 12:08:18 EDT
+#
+# @description : Setup your kotlin.  Manual is for monkeys.
+######################################################################
+#
+#
+
+
+# Use "set -x" to echo commands to stdout with variable expansion
+#set -x
+
+brew reinstall -f jenv
+
+# jenv doesn't bother to create it's own work directory
+mkdir  -p ~/.jenv/versions
+
+brew reinstall -f openjdk@17
+
+jenv add /Library/Java/JavaVirtualMachines/openjdk-17.jdk/Contents/Home
+
+printf '\n\n#############################\n' >> ~/.zshrc
+printf '### setup_kotlin.sh additions below\n' >> ~/.zshrc
+printf '#############################\n\n' >> ~/.zshrc
+
+printf '# Make OpenJDK the first on the path\n' >> ~/.zshrc ;
+printf '"export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"\n'  >> ~/.zshrc ;
+
+printf '# For compilers to find openjdk@17 you may need to set:\n' >> ~/.zshrc
+printf 'export CPPFLAGS="-I/opt/homebrew/opt/openjdk@17/include"\n' >> ~/.zshrc
